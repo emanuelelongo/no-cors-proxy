f68879986fb116e98392846e4c247324f27873ab
Emanuele Longo <emanuele.longo@ynap.com>
2017-03-13 12:31:23 +0100

    created repository

---
+++ bin/index.js
@@ -0,0 +1,12 @@
+const argv = require('yargs').argv
+const NoCorsProxy = require('../lib')
+
+const port = argv.p || argv.port
+const host = argv.h || argv.host
+const target = argv.t || argv.target
+
+const proxy = new NoCorsProxy(port, host, target)
+proxy.start()
+
+console.info(`Proxying http://${proxy.config.host}:${proxy.config.port} to\
+ => ${proxy.config.target}`)
